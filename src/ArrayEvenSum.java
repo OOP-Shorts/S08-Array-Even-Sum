f2fe1124d610c531395dffb00c2ba2726b8ee7c7
ragor114 <69634980+ragor114@users.noreply.github.com>
2021-12-22 10:21:35 +0100

    added solution

--- src/ArrayEvenSum.java
+++ src/ArrayEvenSum.java
@@ -2,7 +2,23 @@ public class ArrayEvenSum {
 
     /* Schreiben Sie eine Methode, die ein int[] als Parameter übergeben bekommt, und alle durch zwei teilbaren Werte aufaddiert und das Ergebnis zurückgibt.*/
     public static int intArrayEvenSum(int[] values) {
-        return 0;
+        // Falls ein leeres Array übergeben wird, wird 0 zurückgegeben.
+        if(values == null || values.length == 0) {
+            return 0;
+        }
+
+        int sum = 0;
+
+        // Das genze Array wird mit einer for-Schleife durchgegangen.
+        for (int i = 0; i < values.length; i++) {
+            // Falls eine Zahl durch zwei teilbar ist liefert die Division durch 2 den Rest 0
+            if(values[i] % 2 == 0) {
+                // Durch zwei teilbare Zahlen werden auf sum addiert.
+                sum += values[i];
+            }
+        }
+
+        return sum;
     }
 
     public static void main(String[] args) {
